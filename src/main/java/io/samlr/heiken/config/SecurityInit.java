a937d8fd3bf0e78f82783c146220ac245db788ac
SamLRR <sergei.ivanov79@list.ru>
2018-11-20 16:03:52 +0300

    add spring core

---
+++ src/main/java/io/samlr/heiken/config/SecurityInit.java
@@ -0,0 +1,6 @@
+package io.samlr.heiken.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
---
+++ web/WEB-INF/views/helloPage.jsp
@@ -0,0 +1,10 @@
+    <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Heiken</title>
+</head>
+<body>
+<h1>Welcome to Heiken.</h1>
+
+</body>
+</html>
